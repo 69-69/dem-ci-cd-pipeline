4772789923383affe2e3676618417f1705e2e9da
69-69 <devmail026@gmail.com>
2025-05-16 12:06:38 -0500

    testing automted regression with PlayWright - end-to-end test cases

---
+++ tests/home.spec.ts
@@ -0,0 +1,15 @@
+import {test, expect} from '@playwright/test';
+
+test('Home page: Automated Regression Testing', async ({page}) => {
+    // Go to the home page
+    await page.goto('/');
+
+    // Expect a title "to contain" a substring
+    await expect(page).toHaveTitle(/Steve Home/);
+
+    // Create a locator
+    const getStarted = page.getByRole('link', {name: 'Home to Blog'});
+
+    // Expect an attribute "to be strictly equal" to the value
+    await expect(getStarted).toHaveAttribute('href', '/blog');
+});
